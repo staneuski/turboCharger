bcdd0d1ab1c98b2c5adadcf2471dcfd0fc9cc542
Stanislau <stas.dmitrievich.stashevskiy@gmail.com>
2018-10-20 23:35:46 +0300

    All for the wheel (to 43rd) point

--- compressor/Main.py
+++ compressor/Main.py
@@ -2,6 +2,8 @@
 # Calculates safety factors for crankshaft and displays the minimum of them
 
 ## Loading input data from project dictionary & some fuctions
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 from __future__ import division
 import math
 
@@ -14,14 +16,18 @@ from compressorDict import(
       alpha, eta_v, phi,
       T_aStagn, p_aStagn, c_0,
       sigma_0, sigma_c, sigma_v,
-      eta_KsStagn, H_KsStagn, phi_flow, dzeta_inlet, relD_1H, relD_1B,
-      i, tau_1,
+      eta_KsStagn, H_KsStagn, phi_flow,
+      dzeta_inlet, dzeta_BA, relW_2rToC_1a, dzeta_TF, alpha_wh, tau_2,
+      relD_1H, relD_1B,
+      i, tau_1, z_K, beta_2Blade,
       E, T_ca,
       proectType
 );
 
 
 ## Converting data to SI from dictionary | Перевод в СИ
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 N_e = N_e*1e03; # -> V
 g_e = g_e*1e-03; # -> kg/(V*h) or g/(kV*h)
 p_aStagn = p_aStagn*1e06; # -> Pa
@@ -29,6 +35,7 @@ D = D*1e-02;      S = S*1e-02; # -> m
 
 
 ## Precalculations
+## ~~~~~~~~~~~~~~~
 
 # Lower heat of combustion for fuel | Низшая теплота сгорания в зависимости от типа ДВС
 if 'SI' in engineType:
@@ -46,7 +53,7 @@ p_e = 0.12*1e03*N_e*strokeNumber/(math.pi*pow(D, 2)*S*n*pistonNumber); # Pa
 # Flow volume | Расход
 G_K = N_e*g_e*l_0*alpha*phi/3600; # kg/s
 
-# Wheel diameter | Диаметр рабочее колесо
+# Wheel diameter | Диаметр рабочего колеса
 if ( issubclass(type(eta_KsStagn), str) ) or \
       ( issubclass(type(H_KsStagn), str) ) or ( issubclass(type(phi_flow), str) ):
       D_2 = 160*G_K + 40; # mm      
@@ -69,7 +76,11 @@ else:
 
 Pi_K = 2.7563;
 
+
 ## Compressor parameters calculation
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ Inlet part | Входное устройство ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 
 # Stagnation parameters of inlet | Параметры торможения на входе (1)
 T_0Stagn = T_aStagn;
@@ -80,90 +91,136 @@ p_0Stagn = sigma_0*p_aStagn;
 T_0 = T_0Stagn - pow(c_0, 2)/2/c_p;
 p_0 = p_0Stagn * pow(T_0/T_0Stagn, k/(k - 1)); # Pa
 
-# Isentropy compression work in compressor | Изоэнтропная работа сжатия в компрессоре (4)
-L_KsStagn = c_p*T_0Stagn*(pow(Pi_K, (k - 1)/k) - 1);
+L_KsStagn = c_p*T_0Stagn*(pow(Pi_K, (k - 1)/k) - 1); # Isentropy compression work in compressor | Изоэнтропная работа сжатия в компрессоре (4)
 
 # Wheel outer diameter circular velocity | Окружная скорость на наружном диаметре колеса (5)
 u_2 = math.sqrt(L_KsStagn / H_KsStagn);
-
 if u_2 >= 550:    
       print 'Wheel outer diameter circular velocity is too high!'
       print 'Try to increase wheel diameter &/or set other ECE parameters'
       exit();
 
-# | Абсолютная скорость потока на входе в рабочее колесо (6)
-c_1 = phi_flow*u_2;
+c_1 = phi_flow*u_2; # | Абсолютная скорость потока на входе в рабочее колесо (6)
 
-# | Температура воздуха на входе в рабочее колесо (7)
-T_1 = T_0 + (pow(c_0, 2) - pow(c_1, 2))/2/c_p;
+T_1 = T_0 + (pow(c_0, 2) - pow(c_1, 2))/2/c_p; # | Температура воздуха на входе в рабочее колесо (7)
 
 # | Расчёт потерь энергии во впускном коллекторе (8)
-deltaL_inlet = dzeta_inlet*pow(c_1, 2)/2;
+if issubclass(type(dzeta_inlet), str):    dzeta_inlet = 0.45;    # default dzeta_inlet
+L_inlet = dzeta_inlet*pow(c_1, 2)/2;
 
-# | Показатель политропы при течении во входном устройстве (9)
-n_1 = ( k/(k - 1) - deltaL_inlet/R/(T_1 - T_0) )/ \
-( k/(k - 1) - deltaL_inlet/R/(T_1 - T_0) - 1);
 
-# | Давление на входе в колесо (10)
-p_1 = p_0*pow(T_1/T_0, n_1/(n_1 - 1));
 
-# | Плотность на входе в колесо (11)
-ro_1 = p_1/R/T_1;
 
-# | Площадь поперечного сечения в колесе (12)
-F_1 = G_K/c_1/ro_1;
+# [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ Compressor wheel | Рабочее колесо ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 
-# | Наружный диаметр колеса на входе D_1H (13)
-D_1H = math.sqrt( 4*F_1/math.pi/(1 - pow(relD_1B/relD_1H, 2)) );
+n_1 = ( k/(k - 1) - L_inlet/R/(T_1 - T_0) )/ \
+( k/(k - 1) - L_inlet/R/(T_1 - T_0) - 1); # | Показатель политропы сжатия в компрессоре (33)
 
-# \ Внутренний диаметер на входе (втулочный диаметр) (14)
-D_1B = relD_1B/relD_1H*D_1H;
+p_1 = p_0*pow(T_1/T_0, n_1/(n_1 - 1)); # | Давление на входе в колесо (10)
+
+ro_1 = p_1/R/T_1; # | Плотность на входе в колесо (11)
+
+F_1 = G_K/c_1/ro_1; # | Площадь поперечного сечения в колесе (12)
+
+D_1H = math.sqrt( 4*F_1/math.pi/(1 - pow(relD_1B/relD_1H, 2)) ); # | Наружный диаметр колеса на входе D_1H (13)
+
+D_1B = relD_1B/relD_1H*D_1H; # | Внутренний диаметер на входе (втулочный диаметр) (14)
 
 # | Наружный диаметр колеса на комперссора на выходе (15)
 D_2 = round( D_1H/relD_1H, 3 );
 print 'The diameter of the wheel is {D_2_mm} mm\n' .format(D_2_mm = D_2*1e+03);
 
-# | Частота вращения турбокомпрессора (16)
-n_tCh = 60*u_2/math.pi/D_2; # 1/min
+n_tCh = 60*u_2/math.pi/D_2; # 1/min, | Частота вращения турбокомпрессора (16)
 
-# | Средний диаметр на входе в колесо
-D_1 = math.sqrt(( pow(D_1B, 2) + pow(D_1H, 2) )/2);
+D_1 = math.sqrt(( pow(D_1B, 2) + pow(D_1H, 2) )/2); # | Средний диаметр на входе в колесо
 
-# | Окружная скорость на среднем диаметре входа (18)
-u_1 = math.pi*D_1*n_tCh/60;
+u_1 = math.pi*D_1*n_tCh/60; # | Окружная скорость на среднем диаметре входа (18)
 
 # | Угол входа потока в рабочее колесо на среднем диамметре в относительном движении (19)
 beta_1 = math.degrees(math.atan( c_1/u_1 ));
-
 if issubclass(type(i), str):    
       print 'Degree of the wheel inlet flow is {0:.3f}' .format(beta_1);
       print 'Now you can set "i", using recomendations'
       exit();
 
-# | Угол установки лопаток на среднем диаметре (20)
-beta_1Blade = beta_1 + i;
+beta_1Blade = beta_1 + i; # | Угол установки лопаток на среднем диаметре (20)
 
 # | Абсолютная скорость при учёте толщины лопаток (21)
-if issubclass(type(tau_1), str):    tau_1 = 0.85; # default tau_1
-
+if issubclass(type(tau_1), str):    tau_1 = 0.85;    # default tau_1
 c_1Tau = c_1 / tau_1;
 
-# | Окружная скорость на наружном диаметре входа в колесо (22)
-u_1H = math.pi*D_1H*n_tCh/60;
+u_1H = math.pi*D_1H*n_tCh/60; # | Угол установки лопаток на среднем диаметре (20)
 
-# | Относительная скорость на наружном диаметре входа в колесо (21)
-w_1H = math.sqrt(pow(c_1Tau, 2) + pow(u_1H, 2));
+w_1H = math.sqrt(pow(c_1Tau, 2) + pow(u_1H, 2)); # | Относительная скорость на наружном диаметре входа в колесо (23)
 
-# | Число маха на наружном диаметре входа в колесо (22)
+# | Число маха на наружном диаметре входа в колесо (24)
 M_w1 = w_1H/math.sqrt(k*R*T_1);
-
 if M_w1 > 0.9:
       print 'Warning!\nMach number (M = {0:.2f}) is too high!' .format(M_w1);
       print 'Try to change "tau_1" &/or other parameters.'
       # exit();
       
-# | Относительная скорость на среднем диаметре входа в колесо (23)
-w_1 = math.sqrt(pow(c_1Tau, 2) + pow(u_1, 2));
+w_1 = math.sqrt(pow(c_1Tau, 2) + pow(u_1, 2)); # | Относительная скорость на среднем диаметре входа в колесо (25)
+
+# | Удельная работа потерь во входном вращающемся направляющем аппарате колеса (26)
+if issubclass(type(dzeta_BA), str):    dzeta_BA = 0.3;   # default dzeta_BA
+L_BA = dzeta_BA*pow(w_1, 2)/2;
+
+# | Радиальная составляющая абсолютной скорости / радиальная составляющая относительной скорости на выходе из колеса (27)
+if issubclass(type(relW_2rToC_1a), str):    relW_2rToC_1a = 1.05;   # default relW_2rToC_1a
+c_2r = relW_2rToC_1a * c_1;
+
+# | Потери на поворот и трение в межлопаточных каналах рабочего колеса (28)
+if issubclass(type(dzeta_TF), str):    dzeta_TF = 0.15;   # default dzeta_TF
+L_TF = dzeta_TF*pow(c_2r, 2)/2;
+
+# | Потери на трение диска колеса о воздух в сумме с вентиляционными потерями (29)
+if issubclass(type(alpha_wh), str):    alpha_wh = 0.06;   # default alpha_wh
+L_TB = alpha_wh*pow(u_2, 2);
+
+mu = 1/(1+2/3*math.pi/z_K \
+    *math.sin(math.radians(beta_2Blade))/(1 - pow(D_1/D_2, 2)) ); # | Коэффициент мощности учитывабщий число лопаток и проч. (31)
+
+T_2 = T_1 + (mu + alpha_wh - 0.5*pow(mu, 2))*pow(u_2, 2)/c_p; # | Температура воздуха за колесом (32)
+
+n_2 = ( k/(k - 1) - (L_BA + L_TF + L_TB)/R/(T_2 - T_1) )/ \
+( k/(k - 1) - (L_BA + L_TF + L_TB)/R/(T_2 - T_1) - 1); # | Показатель политропы сжатия в колесе (33)
+
+p_2 = p_1*pow(T_2/T_1, n_2/(n_2 - 1)); # | Давление на выходе из колеса (34)
+
+ro_2 = p_2/R/T_2; # | Плотность на выходе из колеса (35)
+
+c_2u = mu*(u_2 - c_2r/math.tan(math.radians(beta_2Blade))); # | Окружная составляющая абсолютной скорости на выходе (36)
+
+c_2 = math.sqrt(pow(c_2u, 2) + pow(c_2r, 2)); # | Абсолютная скорость на выходе из колеса
+
+w_2u = u_2 - c_2u; # | Окружная составляющая относительной скорости на выходе из колеса (38)
+
+w_2 = math.sqrt(pow(w_2u, 2) + pow(c_2r, 2)); # | Относительная скорость на выходе из колеса (c_2r = w_2r) (39)
+
+beta_1 = math.degrees(math.acos( w_2u/w_2 )); # | Угол между веторами относительной и окружной скорости на выходе из колеса (40)
+
+alpha_2 = math.degrees(math.acos( c_2u/c_2 )); # | Угол между веторами абсолютной и окружной скорости на выходе из колеса (40)
+
+# | Ширина колеса на выходе из турбины (41)
+if issubclass(type(tau_2), str):    tau_2 = 0.94;   # default tau_2
+b_2 = G_K/math.pi/D_2/c_2r/ro_2/tau_2;
+
+T_2Stagn = T_2 + pow(c_2, 2)/2/c_p; # | Температура заторможенного потока на выходе из колеса (43)
+
+
+
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ Diffuser | Диффузор ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+
+
+
+
+print T_2Stagn;
+
+
+
+
 
 
 
--- compressor/compressorDict.py
+++ compressor/compressorDict.py
@@ -2,13 +2,17 @@
 # Data for calculation
 
 ## Standart paramaters
+## ~~~~~~~~~~~~~~~~~~~
+
 p_a = 0.1013; # MPa, atmospheric pressure | атмосферное давление
 T_a = 293; # К, temperature | температура
 k = 1.4; # isentropy coefficient | коэффициент изоэнтропы
 R = 287.2; # gas constant | газовая постоянная
 c_p = 1005; # J/(kg*K), isobar heat capacity | изобарная теплоёмкость 
 
-##  Engine data | Данные ДВС
+## Engine data | Данные ДВС
+## ~~~~~~~~~~~~~~~~~~~~~~~~
+
 engineType = "DIESEL"; # type of the engine: "DIESEL" or "SI" | вид ДВС: "DIESEL" для ВоС или "SI" для ВЭИ
 strokeNumber = 4; # number of strokes | тактность ДВС
 pistonNumber = 4; # piston number | число цилиндров
@@ -25,34 +29,48 @@ eta_v = 0.963; # admission coefficient | коэффициент наполнен
 phi = 1.025; # expulsion coefficient | коэффициент продувки
 
 ## Compressor parameters
+## ~~~~~~~~~~~~~~~~~~~~~
+
 # Stagnation (*) parameters for inlet
 p_aStagn = p_a; # MPa, stagnation pressure | давление торможения
 T_aStagn = T_a; # К, stagnation pressure | температура торможения
 c_0 = 40; # m/s, intake speed | скорость на входе 
 
 # Losses coefficients | Коэффициенты потерь
-sigma_0 = 0.98; # | Коэффициент потерь полного давления
-sigma_c = 0.985; # | Коэффициент потерь в охладителе
-sigma_v = 0.992; # | Коэффициент потерь в коллекторе
+sigma_0 = 0.98; # | коэффициент потерь полного давления
+sigma_c = 0.985; # | коэффициент потерь в охладителе
+sigma_v = 0.992; # | коэффициент потерь в коллекторе
 
 # Energy conversion efficiency (ECE) & other coefficints | КПД и др. коэффициенты
 # Set "UNKNOWN" if it's unknown | Если какой-либо параметр неизвестен принять его как "UNKNOWN"
 eta_KsStagn = 0.74; # compressor energy conversion efficiency | КПД компрессора
 H_KsStagn = 0.6; # | напорный изоэнтропный КПД
 phi_flow = 0.3; # | коэффициент напора
-dzeta_inlet = 0.04; # 0.03...0.06, | коэффициент сопротивления
+
+# | Коэффициенты потерь
+# Set "DEFAULT" to set default value | Если какой-либо параметр, чтобы принять его по-умолчанию (как "DEFAULT")
+dzeta_inlet = 0.04; # 0.03...0.06, (0.45 - default) | коэффициент сопротивления
+dzeta_BA = 0.26; # 0.1...0.3 (0.2 - default) | коэффициент потерь в направляющем аппарате (26)
+relW_2rToC_1a = "DEFAULT"; # 0.9...1.2 (1.05 - default) | отношение w_2r к с_1a (27)
+dzeta_TF = 0.18; # 0.1...0.2 (0.15 - default) | коэффициент потерь на трение в межлопаточных каналах (28)
+alpha_wh = 0.05; # 0.04...0.08 (0.06 - default) | коэффициент дисковых потерь (29)
+tau_2 = "DEFAULT"; # 0.92...0.96 (0.94 - default) | коэффициент загромождения (42)
+
+# Diameter parameters | Коэффициенты диаметра
 relD_1H = 0.67; # diameter coefficient | коэффициент диаметра
 relD_1B = 0.225; # diameter coefficient | коэффициент диаметра
 
 # Geometric parameters
 # Set "UNKNOWN" if it's unknown | Если какой-либо параметр неизвестен принять его как "UNKNOWN"
-i = 3.913; # 2...6 grad, | угол атаки на входе в колесо
-tau_1 = 0.9; # 0.8...0.9, (0.85 - default) | коэффициент загромождения (стеснения), учитывающий толщину лопаток (по умолчанию - 0.85)
+i = 3.913; # 2...6 grad, (4 - default) | угол атаки на входе в колесо
+tau_1 = 0.9; # 0.8...0.9, (0.85 - default) | коэффициент загромождения (стеснения), учитывающий толщину лопаток
+z_K = 12; # 9...34 | количество лопаток
+beta_2Blade = 75; # | угол лопаток на вызоде из рабочего колеса
 
 ## Other paramaters
+## ~~~~~~~~~~~~~~~~
 E = 0.7; # heat efficiency coefficient | коэффициент тепловой эффективности
 T_ca = 309; # K, coolant temperature | температура охлаждающего агента (воды)
-
 
 
 # Тип проекта (для Д/З задать "HW")
