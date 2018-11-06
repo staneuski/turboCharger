1e44c25ebab4d85fd57c2f012e277918a3e79eb0
Stanislau <stas.dmitrievich.stashevskiy@gmail.com>
2018-11-06 21:09:08 +0300

    Found the mistake in the formula

--- compressor/diffOutTemp.py
+++ compressor/diffOutTemp.py
@@ -11,7 +11,7 @@ def diffOutTemp(b_2, D_2, T_2, c_2, b_4, D_4, guessedT_4, n_4):
     # Precalculations
     q = pow(D_2*b_2/D_4/b_4, 2);
     m = 2/(n_4 - 1);
-    sigma = (k - 1)/1 * pow(c_2, 2)/k/R/T_2;
+    sigma = (k - 1)/2 * pow(c_2, 2)/k/R/T_2;
     
     # Calculation diffuser output temperature
     solvedT_4 = T_2*( 1 + sigma*(1 - q*pow(T_2/guessedT_4, m)) ); 
