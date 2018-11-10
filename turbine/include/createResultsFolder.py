79ef5fdca0cb285f17a5df38b745cad8df0f348b
Stanislau <stas.dmitrievich.stashevskiy@gmail.com>
2018-11-10 18:51:20 +0300

    Edits pictures for turbine report

--- turbine/include/createResultsFolder.py
+++ turbine/include/createResultsFolder.py
@@ -6,7 +6,7 @@ if not os.path.exists("turbineResults"):   os.makedirs("turbineResults");
 
 shutil.copyfile("turbineDict.py",  "turbineResults/turbineDict.py");
 shutil.move("turbineReport.md",    "turbineResults/turbineReport.md");
-# shutil.move("inTurbineWheel.png",  "turbineResults/inTurbineWheel.png");
-# shutil.move("axisCut.png",         "turbineResults/axisCut.png");
-# shutil.move("outTurbineWheel.png", "turbineResults/outTurbineWheel.png");
+shutil.move("axisCut.png",         "turbineResults/axisCut.png");
+shutil.move("inTurbineWheel.png",  "turbineResults/inTurbineWheel.png");
+shutil.move("outTurbineWheel.png", "turbineResults/outTurbineWheel.png");
 shutil.copyfile("../programFiles/turbine/i-sPlot.png", "turbineResults/i-sPlot.png");
