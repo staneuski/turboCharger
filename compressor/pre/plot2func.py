260f5b68f561e6750c2b693a06caecbd942aff55
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2020-12-23 23:14:14 +0300

    Unify plot2func functions

--- compressor/pre/plot2func.py
+++ compressor/pre/plot2func.py
@@ -9,93 +9,85 @@ def value_btw_two_plots(balance_coef, upper_value, lower_value):
     return balance_coef*upper_value + (1 - balance_coef)*lower_value
 
 
-def value_range_of_plot(balance_coef, delta, lower_value):
+def value_range_from_line(balance_coef, delta, lower_value):
     """Set value using the interpolated function, range
     and balance coefficient.
     """
 
     return lower_value + balance_coef*delta
 
+def exit_wheel_diameter_error(D, error_no=0):
+    """Stop the program when wheel diameter is out of range"""
+    if (D < 0.04) | (D > 0.8):
+        return exit(f"\033[91mERROR {int(error_no)}: Wheel diameter "
+                    f"{D*1e+02:.0f} is out of range (4…80) cm!\n"
+                    "No data for that wheel diameter.\n")
+
 
 # Coeficients
 def H_plot2func(balance_coef, D):
-
-    import math
-
-    if (D < 0.04) | (D > 0.8):
-        exit(f"\033[91mERROR 5: Wheel diameter {D*1e+02:.0f} "
-             "is out of range (4…80) cm!\nNo data for that wheel diameter.\n")
-
+    from math import log
+    exit_wheel_diameter_error(D, error_no=5)
     return value_btw_two_plots(
         balance_coef,
-        0.0485923*math.log(D) + 0.80737,
-        -2.28399*D**3 + 5.05698*D**3 - 4.02875*D**2 + 1.40783*D + 0.478618)
+        0.0485923*log(D) + 0.80737,
+        -2.28399*D**3 + 5.05698*D**3 - 4.02875*D**2 + 1.40783*D + 0.478618
+    )
 
 
 def phi_plot2func(balance_coef, D):
-    if (D < 0.04) | (D > 0.8):
-        exit(f"\033[91mERROR 6: Wheel diameter {D*1e+02:.0f} "
-             "is out of range (4…80) cm!\nNo data for that wheel diameter.\n")
-
+    exit_wheel_diameter_error(D, error_no=6)
     return value_btw_two_plots(
         balance_coef,
         1.21576*D**3 - 2.47517*D**3 + 1.77501*D**2 - 0.528671*D + 0.365277,
-        0.84364*D**3 - 1.74951*D**3 + 1.27749*D**2 - 0.384933*D + 0.292871)
+        0.84364*D**3 - 1.74951*D**3 + 1.27749*D**2 - 0.384933*D + 0.292871
+    )
 
 
 def eta_plot2func(balance_coef, D):
-    if (D < 0.04) | (D > 0.8):
-        exit(f"\033[91mERROR 15: Wheel diameter {D*1e+02:.0f} "
-             "is out of range (4…80) cm!\nNo data for that wheel diameter.\n")
-
+    exit_wheel_diameter_error(D, error_no=15)
     return value_btw_two_plots(
         balance_coef,
         -1.41956*D**3 + 3.59445*D**3 - 3.19974*D**2 + 1.20019*D + 0.70186,
-        -2.54297*D**3 + 5.79527*D**3 - 4.69133*D**2 + 1.64249*D + 0.603052)
+        -2.54297*D**3 + 5.79527*D**3 - 4.69133*D**2 + 1.64249*D + 0.603052
+    )
 
 
 def relD_1H_plot2func(balance_coef, D):
-    if (D < 0.04) | (D > 0.8):
-        exit(f"\033[91mERROR 13: Wheel diameter {D*1e+02:.0f} "
-             "is out of range (4…80) cm!\nNo data for that wheel diameter.\n")
-
+    exit_wheel_diameter_error(D, error_no=13)
     return value_btw_two_plots(
         balance_coef,
         -0.05*D + 0.712,
-        -0.35*D + 0.634)
+        -0.35*D + 0.634
+    )
 
 
 def relD_1B_plot2func(balance_coef, D):
-    if (D < 0.04) | (D > 0.8):
-        exit(f"\033[91mERROR 13: Wheel diameter {D*1e+02:.0f} "
-             "is out of range (4…80) cm!\nNo data for that wheel diameter.\n")
-
-    return value_range_of_plot(
+    exit_wheel_diameter_error(D, error_no=13)
+    return value_range_from_line(
         balance_coef,
         0.06,
-        0.260417*D**2 + 0.0280449*D + 0.198365)
+        0.260417*D**2 + 0.0280449*D + 0.198365
+    )
 
 
 def relSpeeds_plot2func(balance_coef, D):
-    if (D < 0.04) | (D > 0.8):
-        exit(f"\033[91mERROR 27: Wheel diameter {D*1e+02:.0f} "
-             "is out of range (4…80) cm!\nNo data for that wheel diameter.\n")
+    exit_wheel_diameter_error(D, error_no=27)
 
     return value_btw_two_plots(
         balance_coef,
         1.71023 *D**3 - 3.78212*D**3 + 2.99991*D**2 - 1.05122*D + 1.23182,
-        -6.37691*D**3 + 12.3393*D**3 - 8.57209*D**2 + 2.54654*D + 0.734892)
+        -6.37691*D**3 + 12.3393*D**3 - 8.57209*D**2 + 2.54654*D + 0.734892
+    )
 
 
 def z_plot2func(balance_coef, D):
-    if (D < 0.04) | (D > 0.8):
-        exit(f"\033[91mERROR 30: Wheel diameter {D*1e+02:.0f} "
-             "is out of range (4…80) cm!\nNo data for that wheel diameter.\n")
-
-    return value_range_of_plot(
+    exit_wheel_diameter_error(D, error_no=27)
+    return value_range_from_line(
         balance_coef,
         9,
-        141.063*D**3 - 228.472*D**2 + 134.588*D + 2.44075)
+        141.063*D**3 - 228.472*D**2 + 134.588*D + 2.44075
+    )
 
 
 # ''' (C) 2018-2020 Stanislau Stasheuski '''
--- turbine/pre/plot2func.py
+++ turbine/pre/plot2func.py
@@ -14,7 +14,7 @@ def value_range_of_plot(balance_coef, delta, lower_value):
 
     return lower_value + balance_coef*delta
 
-def checkWheelDiameter(D):
+def exit_wheel_diameter_error(D):
     if (D < 0.04) | (D > 0.24):
         exit('\033[91mERROR: No data for that wheel diameter!\n'
              f'It equals {D*1e+02:.0f} cm but must be from 4 to 24 cm.\n'
@@ -23,43 +23,43 @@ def checkWheelDiameter(D):
 
 # Coeficients
 def eta_plot2func(balance_coef, D):
-    checkWheelDiameter(D)
+    exit_wheel_diameter_error(D)
     return value_btw_two_plots(balance_coef,
                                41.6424*D**3 - 23.8782*D**2 + 4.65788*D + 0.502197,
                                47.3485*D**3 - 28.2561*D**2 + 5.69391*D + 0.318636)
 
 def alpha_plot2func(balance_coef, D):
-    checkWheelDiameter(D)
+    exit_wheel_diameter_error(D)
     return value_btw_two_plots(balance_coef,
                                607.605*D**3 - 414.379*D**2 + 99.4706*D + 21.6024,
                                81.285 *D**3 - 290.388*D**2 + 120.468*D + 10.7)
 
 def phi_plot2func(balance_coef, D):
-    checkWheelDiameter(D)
+    exit_wheel_diameter_error(D)
     return value_btw_two_plots(balance_coef,
                                -1.4881*D**2 + 0.541667*D + 0.931414,
                                -4.7619*D**2 + 1.83333*D + 0.794286)
 
 def psi_plot2func(balance_coef, D):
-    checkWheelDiameter(D)
+    exit_wheel_diameter_error(D)
     return value_btw_two_plots(balance_coef,
                                -6.35417*D**2 + 2.35417*D + 0.746,
                                -5.83333*D**2 + 2.43333*D + 0.662)
 
 def ksi_plot2func(balance_coef, D):
-    checkWheelDiameter(D)
+    exit_wheel_diameter_error(D)
     return value_range_of_plot(balance_coef,
                                0.09,
                                -2.38095*D**2 + 1.16667*D + 0.527143)
 
 def relD_1H(balance_coef, D):
-    checkWheelDiameter(D)
+    exit_wheel_diameter_error(D)
     return value_btw_two_plots(balance_coef,
                                4.40039*D**2 - 2.22469*D + 0.981044,
                                73.7665*D**3 - 37.701*D**2 + 5.48361*D + 0.39897)
 
 def relD_2B(balance_coef, D):
-    checkWheelDiameter(D)
+    exit_wheel_diameter_error(D)
     return value_range_of_plot(balance_coef,
                                0.1,
                                2.61905*D**2 - 0.533333*D + 0.227143)
